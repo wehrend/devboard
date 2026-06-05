7de64ecbdfb1eb269e29fe0886a5f14bdfe16e35
Sven Wehrend <swehrend@ant-neuro.com>
2026-06-05 19:54:16 +0200

    implement board details, part 3

--- src/pages/BoardDetail/BoardDetail.tsx
+++ src/pages/BoardDetail/BoardDetail.tsx
@@ -1,3 +1,67 @@
+import { ArrowLeft, Pencil, Check, X } from "lucide-react"
+import { Input } from "../../components/ui/input"
+
+import { Button } from "../../components/ui/button"
+import { Link } from "react-router-dom"
+import { useState } from "react"
+
 export default function BoardDetail() {
-  return "BoardDetail";
+  const [isEditingBoardName, setEditingBoardName] = useState(false)
+  const [boardName, setBoardName] = useState("Name des Boards")
+
+  function renderBoardDetailHeader() {
+    if (isEditingBoardName) {
+      return (
+        <>
+          <Input
+            value={boardName}
+            className="w-64"
+            onChange={(event) => setBoardName(event.target.value)}
+          />
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setEditingBoardName(false)}
+          >
+            <Check />
+          </Button>
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setEditingBoardName(false)}
+          >
+            <X />
+          </Button>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <h1 className="text-2xl font-bold">{boardName}</h1>
+          <Button
+            variant="ghost"
+            size="icon-lg"
+            onClick={() => setEditingBoardName(true)}
+          >
+            <Pencil />
+          </Button>
+        </>
+      )
+    }
+  }
+
+  return (
+    <>
+      <div className="container">
+        <div className="flex flex-row items-center gap-2">
+          <Link to={"/boards"}>
+            <Button variant="ghost" size="icon-lg">
+              <ArrowLeft />
+            </Button>
+          </Link>
+          {renderBoardDetailHeader()}
+        </div>
+      </div>
+    </>
+  )
 }
