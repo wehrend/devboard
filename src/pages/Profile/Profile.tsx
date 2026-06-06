0e9e9f4359ecf19ebea9f5ef9c0c94cead0457ef
Sven Wehrend <swehrend@ant-neuro.com>
2026-06-06 19:14:01 +0200

    implement profile page (or part 4)

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,3 +1,34 @@
+import { use, useState } from "react"
+import { Button } from "src/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "src/components/ui/card"
+
 export default function Profile() {
-  return "Profile"
+  const [username, setUsername] = useState("Sven")
+
+  return (
+    <div className="mx-auto max-w-md">
+      <h1 className="mb-6 text-2xl font-bold">Profil</h1>
+      <Card>
+        <CardHeader>
+          <CardTitle>Benutzernamen ändern</CardTitle>
+          <CardDescription>
+            Ändere deinen Anzeigenamen für das Kanban-Board
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col gap-1">
+            <label>Benutzername</label>
+            <input id="username" value={username}/>
+            <Button className="w-fit">Speichern</Button>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
