8d2fd40d128f7240afff2aef3393763c43a85e28
Sven Wehrend <swehrend@ant-neuro.com>
2026-06-18 14:42:54 +0200

    implement dialog for boards (chapter 8)

--- src/pages/BoardOverview/BoardOverview.tsx
+++ src/pages/BoardOverview/BoardOverview.tsx
@@ -5,6 +5,18 @@ import BoardCard from "./components/BoardCard"
 import type { Board } from "src/types/board.type"
 import { Plus } from "lucide-react"
 
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "src/components/ui/dialog"
+import { Input } from "src/components/ui/input"
+
 export default function BoardOverview() {
   const [boards, setBoards] = useState<Board[]>([
     {
@@ -14,14 +26,44 @@ export default function BoardOverview() {
     },
   ])
 
+  const [boardNameInput, setBoardNameInput] = useState("Neues Board")
+
   return (
     <>
       <div className="place-content-betwee flex flex-row">
         <h1 className="text-xl font-bold">Meine Boards</h1>
-        <Button>
-          <Plus />
-          Neues Board
-        </Button>
+        <Dialog>
+          <DialogTrigger>
+            <Button>
+              <Plus />
+              Neues Board
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Neues Board erstellen</DialogTitle>
+              <DialogDescription>
+                Gib dem Board einen Namen. Es werden automatisch drei Spalten
+                angelegt (ToDo, In Progress, Done).
+              </DialogDescription>
+            </DialogHeader>
+            <Input
+              onChange={(e) => setBoardNameInput(e.target.value)}
+              id="name-1"
+              name="name"
+              defaultValue="Neues Board"
+              value={boardNameInput}
+            />
+            <DialogFooter>
+              <DialogClose>
+                <Button variant={"outline"}>Abbrechen</Button>
+              </DialogClose>
+              <DialogClose>
+                <Button>Speichern</Button>
+              </DialogClose>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div className="grid grid-cols-3 gap-4">
         {boards.map((board) => {
